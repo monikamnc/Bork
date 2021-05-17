f9fd74369365d076bb8b92f7224303f63bf8c2cc
Monica <monicamartin13@hotmail.com>
2021-05-17 18:01:20 +0200

    Define Exit Class

--- BorkMoni/Exit.h
+++ BorkMoni/Exit.h
@@ -1,5 +1,23 @@
 #pragma once
-class Exit
-{
-};
+#include <string>
+#include "Entity.h"
 
+class Room;
+
+using namespace std;
+
+class Exit : public Entity
+{
+public:
+	Exit(const char* name, const char* opposite_name, const char* description, Room* origin, Room* destination, bool one_way = false);
+	~Exit();
+
+
+	//Variables
+	bool one_way;
+	bool closed;
+	bool locked;
+	string opposite_name;
+	Room* destination;
+	Entity* key;
+};
