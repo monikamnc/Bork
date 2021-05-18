de7e8730384986dc45b26732c7bb26787e7bfe39
Monica <monicamartin13@hotmail.com>
2021-05-18 18:16:39 +0200

    Create Class Item

---
+++ BorkMoni/Item.cpp
@@ -0,0 +1,13 @@
+#include "Item.h"
+
+
+Item::Item(const char* title, const char* description, Entity* parent, bool isClosed, ItemType item_type) : Entity(title, description, parent), usable(NULL), item_type(item_type)
+{
+	type = ITEM;
+	min_value = max_value = 0;
+	closed = isClosed;
+}
+
+
+Item::~Item()
+{}
---
+++ BorkMoni/Item.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include "Entity.h"
+
+
+enum ItemType
+{
+	GENERIC,
+	WEAPON,
+	ARMOUR
+};
+
+class Item : public Entity
+{
+
+public:
+	Item(const char* name, const char* description, Entity* parent, bool isClosed, ItemType item_type = GENERIC);
+	~Item();
+
+
+
+
+	//Variables
+	int min_value;
+	int max_value;
+	bool closed;
+	Entity* usable;
+	ItemType item_type;
+};
+
